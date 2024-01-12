4039654c0de4a95ad2812181bb044a49f6aebb09
Cocera <alcocera@gmail.com>
2024-01-12 14:02:37 +0100

    desinstalled chakra

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,35 +2,16 @@ import "./Nav";
 import { NavLink } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
-import {
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    MenuItemOption,
-    MenuGroup,
-    MenuOptionGroup,
-    MenuDivider,
-    Button
-} from '@chakra-ui/react'
-
 const Nav = () => {
     const { loginWithRedirect } = useAuth0();
     return (
-        <Menu>
-            <MenuButton as={Button} m={2} onClick={() => window.scrollTo(0, 0)}>
-                Home
-            </MenuButton>
-            <MenuButton as={Button} m={2} onClick={() => loginWithRedirect()}>
-                Iniciar sesión
-            </MenuButton>
-            <MenuButton as={Button} m={2}>
-                Perfil
-            </MenuButton>
-            <MenuButton as={Button} m={2}>
-                Incidencias
-            </MenuButton>
-        </Menu>
+        <>
+            <nav>
+                <NavLink onClick={() => window.scrollTo(0, 0)}>Home</NavLink>
+                <NavLink onClick={() => loginWithRedirect()}>Iniciar sesión</NavLink>
+                <NavLink>Perfil</NavLink>
+            </nav>
+        </>
     )
 };
 
