e79bfeced43511a5cde83da681815704cc35c473
Frances Morales Velilla <frances0688@gmail.com>
2024-01-12 14:22:39 +0100

    Revert "Create Power Bi embedding component."
    This reverts commit 7f1be4ba8ef2ed9178fa9ef698f0f29960bbf520.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,12 @@
 import "./Home.scss";
 import Nav from "../../components/Nav/Nav";
 import Footer from "../../components/Footer/Footer";
-import PowerBi from "../../components/PowerBi/PowerBi";
 
 const Homepage = () => {
 	return (
 		<>
 			<Nav />
 			<h1>Esto es la HOMEPAGE</h1>
-
-			<h1>Power BI Report Embedded in React</h1>
-			<PowerBi />
-
 			<Footer />
 		</>
 	);
