a921951c60c7459b23db530f84d95f4cf4b130ca
Frances Morales Velilla <frances0688@gmail.com>
2024-01-12 14:55:56 +0100

    Create Power Bi embedding component.

---
+++ src/components/PowerBi/PowerBi.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const PowerBi = () => {
+	return (
+		<div>
+			<iframe
+				title="Sample Report Demo"
+				width="1140"
+				height="541.25"
+				src="https://playground.powerbi.com/sampleReportEmbed"
+				frameborder="0"
+				allowFullScreen="true"></iframe>
+		</div>
+	);
+};
+
+export default PowerBi;
