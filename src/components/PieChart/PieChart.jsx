1da7f0f991accddfccc50ee3230bf1328aa7a474
Frances Morales Velilla <frances0688@gmail.com>
2024-01-14 01:55:43 +0100

    Add Recharts library for Pie Chart.

--- src/components/PieChart/PieChart.jsx
+++ src/components/PieChart/PieChart.jsx
@@ -1,69 +1,81 @@
-import { Pie } from "@ant-design/plots";
+import { PieChart, Pie, Tooltip, Cell, Label } from "recharts";
 
-const PieChart = () => {
+const DonutChart = () => {
 	const data = [
-		{
-			type: "A",
-			value: 27,
-		},
-		{
-			type: "B",
-			value: 25,
-		},
-		{
-			type: "C",
-			value: 18,
-		},
-		{
-			type: "D",
-			value: 15,
-		},
-		{
-			type: "E",
-			value: 10,
-		},
-		{
-			type: "F",
-			value: 5,
-		},
+		{ name: "Icon1", value: 400 },
+		{ name: "Icon2", value: 700 },
+		{ name: "Icon3", value: 200 },
+		{ name: "Icon4", value: 1000 },
+		{ name: "Icon5", value: 200 },
+		{ name: "Icon6", value: 1000 },
 	];
-	const config = {
-		appendPadding: 10,
-		data,
-		angleField: "value",
-		colorField: "type",
-		radius: 1,
-		innerRadius: 0.7,
-		label: {
-			type: "inner",
-			offset: "-50%",
-			content: "{value}",
-			style: {
-				textAlign: "center",
-				fontSize: 14,
-			},
-		},
-		interactions: [
-			{
-				type: "element-selected",
-			},
-			{
-				type: "element-active",
-			},
-		],
-		statistic: {
-			title: false,
-			content: {
-				style: {
-					whiteSpace: "pre-wrap",
-					overflow: "hidden",
-					textOverflow: "ellipsis",
-				},
-				content: "AntV\nG2Plot",
-			},
-		},
+
+	const colors = [
+		"#024B59",
+		"#AB172F",
+		"#1ABC7B",
+		"#FFCC4D",
+		"#2BB0C9",
+		"#A5BEAC",
+	];
+
+	const radian = Math.PI / 180;
+	const renderCustomizedLabel = ({
+		cx,
+		cy,
+		midAngle,
+		innerRadius,
+		outerRadius,
+		percent,
+	}) => {
+		const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+		const x = cx + radius * Math.cos(-midAngle * radian);
+		const y = cy + radius * Math.sin(-midAngle * radian);
+
+		return (
+			<text
+				x={x}
+				y={y}
+				fill="white"
+				textAnchor={x > cx ? "start" : "end"}
+				dominantBaseline="central">
+				{`${(percent * 100).toFixed(0)}%`}
+			</text>
+		);
 	};
-	return <Pie {...config} />;
+
+	return (
+		<>
+			<h1>Address</h1>
+			<div
+				style={{
+					textAlign: "center",
+					margin: "auto 10%",
+				}}>
+				<PieChart width={700} height={700}>
+					<Tooltip />
+					<Pie
+						data={data}
+						dataKey="value"
+						outerRadius={250}
+						innerRadius={150}
+						fill="green"
+						labelLine={false}
+						label={({ name, value }) => `${name}: ${value}`}
+						// label={renderCustomizedLabel}
+					>
+						<Label value="Total" position="center" />
+						{data.map((entry, index) => (
+							<Cell
+								key={`cell-${index}`}
+								fill={colors[index % colors.length]}
+							/>
+						))}
+					</Pie>
+				</PieChart>
+			</div>
+		</>
+	);
 };
 
-export default PieChart;
+export default DonutChart;
