3b3cf127f98d8330cbb0855fb48c969b804b82b4
Luciana Boasso <luboasso16@gmail.com>
2024-01-15 15:07:10 +0100

    translate perfil

--- src/App.jsx
+++ src/App.jsx
@@ -10,42 +10,45 @@ import IncidencesList from "./pages/IncidencesList/IncidencesList";
 import CommunitySearch from "./pages/CommunitySearch/COmmunitySearch";
 import IncidenceInfo from "./pages/IncidenceInfo/IncidenceInfo";
 
-
 function App() {
-	return (
-		<>
-			<Router>
-				<Routes>
-					<Route path="/" element={<Home />}></Route>
-					<Route
-						path="/comunidad/cuenta"
-						path="/comunidad/cuenta"
-						element={<CommunityAccount />}></Route>
-					<Route path="/profile" element={<Profile />}></Route>
-					<Route
-						path="/comunidad/cuenta/historial"
-						path="/comunidad/cuenta/historial"
-						element={<CommunityAccountHistory />}></Route>
-					<Route
-						path="/incidencia/busqueda"
-						element={<Search />}></Route>
-					<Route
-						path="/comunidad/busqueda"
-						element={<Search />}></Route>
-					<Route
-						path="/comunidad/incidencias/activas"
-						element={<IncidencesList />}></Route>
-					<Route
-						path="/comunidad/incidencias/pendientes"
-						element={<IncidencesList />}></Route>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/comunidad/cuenta"
+            element={<CommunityAccount />}
+          ></Route>
+          <Route path="/perfil" element={<Profile />}></Route>
+          <Route
+            path="/comunidad/cuenta/historial"
+            element={<CommunityAccountHistory />}
+          ></Route>
+          <Route path="/incidencia/busqueda" element={<Search />}></Route>
+          <Route path="/comunidad/busqueda" element={<Search />}></Route>
+          <Route
+            path="/comunidad/incidencias/activas"
+            element={<IncidencesList />}
+          ></Route>
+          <Route
+            path="/comunidad/incidencias/pendientes"
+            element={<IncidencesList />}
+          ></Route>
 
-					<Route path="/buscador/comunidad" element={<CommunitySearch />}></Route>
-					<Route path="/comunidad/incidencias/info" element={<IncidenceInfo />}></Route>
-					{/* <Route path="*" element={<NotFound />}></Route> */}
-				</Routes>
-			</Router>
-		</>
-	);
+          <Route
+            path="/buscador/comunidad"
+            element={<CommunitySearch />}
+          ></Route>
+          <Route
+            path="/comunidad/incidencias/info"
+            element={<IncidenceInfo />}
+          ></Route>
+          {/* <Route path="*" element={<NotFound />}></Route> */}
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
