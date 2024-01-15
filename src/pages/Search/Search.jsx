06af02d3364b89fc69ca6a6f7dbaa3fa430e1931
Frances Morales Velilla <frances0688@gmail.com>
2024-01-15 16:28:06 +0100

    Correct Search component naming.

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,42 +1,40 @@
-import React from "react";
 import CardImage from "../../components/CardImage/CardImage";
 import { LeftOutlined } from "@ant-design/icons";
 import SearchBar from "../../components/SearchBar/SearchBar";
-import { useLocation } from 'react-router-dom';
-
+import { useLocation } from "react-router-dom";
 
 const Search = () => {
-    const location = useLocation();
+	const location = useLocation();
 
-    if (location.pathname === '/comunidad/busqueda') {
-        return (
-            <>
-                <a>
-                    {" "}
-                    <LeftOutlined />
-                    Comunidades
-                </a>
-                <SearchBar />
-                <CardImage />
-                <CardImage />
-            </>
-        )
-    }
+	if (location.pathname === "/comunidad/busqueda") {
+		return (
+			<>
+				<a>
+					{" "}
+					<LeftOutlined />
+					Comunidades
+				</a>
+				<SearchBar />
+				<CardImage />
+				<CardImage />
+			</>
+		);
+	}
 
-    if (location.pathname === '/incidencia/busqueda') {
-        return (
-            <>
-                <a>
-                    {" "}
-                    <LeftOutlined />
-                    Incidencias
-                </a>
-                <SearchBar />
-                <CardImage />
-                <CardImage />
-            </>
-        )
-    }
+	if (location.pathname === "/incidencia/busqueda") {
+		return (
+			<>
+				<a>
+					{" "}
+					<LeftOutlined />
+					Incidencias
+				</a>
+				<SearchBar />
+				<CardImage />
+				<CardImage />
+			</>
+		);
+	}
 };
 
 export default Search;
