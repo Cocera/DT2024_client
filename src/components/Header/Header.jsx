dc31a3a2873ace3e94215b6371ab3943710ffdca
Cocera <alcocera@gmail.com>
2024-01-15 22:23:10 +0100

    fixed import logo in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import './Header.scss';
 import Subheader from "./Subheader/Subheader";
+import logoImg from "../../assets/logo_sinfondo.png"
 
 const Header = (params) => {
   return (
     <>
       <div className="main-header">
-        <img className="logo" src="src/assets/logo_sinfondo.png" alt="logo" />
+        <img className="logo" src={logoImg} alt="logo" />
       </div>
       {params.title ? <Subheader title={params.title} community={params.community} /> : ""}
     </>
