bc1e14058386e9c461cdc69100dcc5e5eb098047
Frances Morales Velilla <frances0688@gmail.com>
2024-01-16 01:59:23 +0100

    Pass incidence info thorugh props to other components in page and add community images..

---
+++ src/components/IncidenceDetail/IncidenceDetail.jsx
@@ -0,0 +1,20 @@
+const IncidenceDetail = ({ incidence }) => {
+	console.log(incidence);
+	return (
+		<div className="info">
+			<h6 className="title">{incidence.title}</h6>
+			<p>{incidence.description}</p>
+
+			<h6>Publicada por:</h6>
+			<p>{incidence.owner}</p>
+
+			<h6>Proveedor contactado: </h6>
+			<p>{incidence.provider}</p>
+
+			<h6>Fecha de incidencia:</h6>
+			<p>{incidence.date}</p>
+		</div>
+	);
+};
+
+export default IncidenceDetail;
--- src/components/IncidenceImage/IncidenceImage.jsx
+++ src/components/IncidenceImage/IncidenceImage.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import "./IncidenceImage.scss"
-import { Image } from 'antd';
+import React from "react";
+import "./IncidenceImage.scss";
+import { Image } from "antd";
 
-const IncidenceImage = (params) => {
-  if (!params.src) {
-    return (
-        <Image className='img' src='src/assets/Images/humedadg.jpeg' width={params.width} height={params.height} />
-    )
-}
-return (
-    <>
-        <Image src={params.src} width={params.width} height={params.height} style={{preview:false}} />
-    </>
-)
-}
+const IncidenceImage = ({ incidence }) => {
+	if (!incidence.image || incidence.image == "") {
+		return (
+			<Image
+				className="img"
+				src="/src/assets/Images/humedadg.jpeg"
+				width="100vw"
+				height="auto"
+			/>
+		);
+	}
+	return (
+		<>
+			<Image
+				className="img"
+				src={incidence.image}
+				width="100vw"
+				height="auto"
+				style={{ preview: false }}
+			/>
+		</>
+	);
+};
 
-export default IncidenceImage
+export default IncidenceImage;
