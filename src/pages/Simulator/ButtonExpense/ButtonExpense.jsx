838ae68e26c66bf806c80966bb65b5aba41f085c
Cocera <alcocera@gmail.com>
2024-01-17 18:54:33 +0100

    simulator working

--- src/pages/Simulator/ButtonExpense/ButtonExpense.jsx
+++ src/pages/Simulator/ButtonExpense/ButtonExpense.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import "./ButtonExpense.scss";
 
-const OwnerCard = (params) => {
+const ButtonExpense = ({ icon, title, amount, years, date, onClick, className }) => {
     return (
-        <>
-
-            <button className="button-future-expenses">
-                <div className="secondary-color-bg container-future-expenses-one">
-                    <span class="material-symbols-rounded">{params.icon}</span>
-                </div>
-                <div className="container-future-expenses-two">
-                    <h3 className="text-button-expense secondary-color">{params.title}</h3>
-                    <p className="text-button-expense grey-color">Cada {params.years} años</p>
-                    <h3 className="text-button-expense secondary-color">{params.amount}€</h3>
-                    <p className="text-button-expense grey-color">Aprox. {params.date}</p>
-                </div>
-            </button>
-        </>
-    )
+        <button className={`button-future-expenses ${className}`} onClick={onClick}>
+            <div className="secondary-color-bg container-future-expenses-one">
+                <span className="material-symbols-rounded">{icon}</span>
+            </div>
+            <div className="container-future-expenses-two">
+                <h3 className="text-button-expense secondary-color">{title}</h3>
+                <p className="text-button-expense grey-color">Cada {years} años</p>
+                <h3 className="text-button-expense secondary-color">{amount}€</h3>
+                <p className="text-button-expense grey-color">Aprox. {date}</p>
+            </div>
+        </button>
+    );
 };
 
-export default OwnerCard;
+export default ButtonExpense;
--- src/pages/Simulator/OwnerCard/OwnerCard.jsx
+++ src/pages/Simulator/OwnerCard/OwnerCard.jsx
@@ -2,25 +2,28 @@ import React from "react";
 import "./OwnerCard.scss";
 import { Divider } from 'antd';
 
-const OwnerCard = (params) => {
+const OwnerCard = ({ ownerId, actualAmount, newAmount }) => {
+
+    const newAmountRounded = Math.round(newAmount);
+
     return (
         <article className="owner-card">
             <div className="owner-id secondary-color-bg">
-                <h2>{params.ownerId}</h2>
+                <h2>{ownerId}</h2>
             </div>
             <div className="info-container">
                 <div className="info-cuota">
                     <p className="grey-color">Cuota actual:</p>
-                    <h2 className="grey-color">{params.actualAmount}€</h2>
+                    <h2 className="grey-color">{actualAmount}€</h2>
                 </div>
                 <Divider type="vertical" style={{marginLeft: 24, marginRight: 24, height: 55}} />
                 <div className="info-cuota">
                     <p className="grey-color">Cuota estimada:</p>
-                    <h2 className="secondary-color">{params.newAmount}€</h2>
+                    <h2 className="secondary-color">{newAmountRounded}€</h2>
                 </div>
             </div>
         </article>
-    )
+    );
 };
 
 export default OwnerCard;
