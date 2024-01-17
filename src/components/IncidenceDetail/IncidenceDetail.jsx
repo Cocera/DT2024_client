7328342a8879ec4f867ebcfdb20e31edd12eb576
Luciana Boasso <luboasso16@gmail.com>
2024-01-17 23:51:39 +0100

    hide edit button based on route

--- src/components/IncidenceDetail/IncidenceDetail.jsx
+++ src/components/IncidenceDetail/IncidenceDetail.jsx
@@ -1,14 +1,26 @@
-import "../../pages/IncidenceInfo/IncidenceInfo.scss";
+import React from "react";
+import { useLocation } from "react-router-dom";
 import editButtonImage from "../../assets/Images/edit-button.png";
 import "../IncidenceDetail/IncidenceDetail.scss"
 
 const IncidenceDetail = ({ incidence }) => {
-  console.log(incidence);
+  const location = useLocation();
+
+  const isAprobadaRoute =
+    location.pathname === "/comunidad/incidencias/info/aprobada";
+
   return (
     <div className="info">
       <div className="incidence-detail-title">
         <h6 className="title">{incidence.title}</h6>
-        <img src={editButtonImage} alt="edit-button" className="edit-button" />
+
+        {!isAprobadaRoute && (
+          <img
+            src={editButtonImage}
+            alt="edit-button"
+            className="edit-button"
+          />
+        )}
       </div>
       <p>{incidence.description}</p>
 
