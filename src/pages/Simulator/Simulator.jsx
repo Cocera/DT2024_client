dcee9762b9b26177502e54effaf73f254553ef2d
Cocera <alcocera@gmail.com>
2024-01-17 16:09:21 +0100

    Subheader

--- src/pages/Simulator/Simulator.jsx
+++ src/pages/Simulator/Simulator.jsx
@@ -5,33 +5,35 @@ import ButtonExpense from "./ButtonExpense/ButtonExpense";
 import OwnerCard from "./OwnerCard/OwnerCard";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import { Flex } from "antd";
+import Footer from "../../components/Footer/Footer";
 
 const Simulator = () => {
     return (
         <>
             <Header title="Simulador" community="Av. Reino de Valencia, 87" />
             <main>
-            <Flex justify="" align="center" vertical="true" style={{ paddingLeft: 18, paddingRight: 18 }}>
-                <div className="container-future-expenses margin-top-28">
-                    <h4 className="secondary-color">Gastos previstos</h4>
-                    <div className="container-buttons margin-top-28">
-                        <ButtonExpense icon="elevator" title="Ascensor" amount="3000" years="5" date="07/2029" />
-                        <ButtonExpense icon="phone_enabled" title="Telefonillos" amount="5000" years="7" date="07/2029" />
+                <Flex justify="" align="center" vertical="true" style={{ paddingLeft: 18, paddingRight: 18 }}>
+                    <div className="container-future-expenses margin-top-28">
+                        <h4 className="secondary-color">Gastos previstos</h4>
+                        <div className="container-buttons margin-top-28">
+                            <ButtonExpense icon="elevator" title="Ascensor" amount="3000" years="5" date="07/2029" />
+                            <ButtonExpense icon="phone_enabled" title="Telefonillos" amount="5000" years="7" date="07/2029" />
+                        </div>
                     </div>
-                </div>
-                <div className="margin-top-28">
-                <SearchBar />
-                </div>
-                <div className="margin-top-28">
-                    <OwnerCard ownerId="A1" actualAmount="40" newAmount="70" />
-                    <OwnerCard ownerId="A2" actualAmount="40" newAmount="70" />
-                    <OwnerCard ownerId="B1" actualAmount="40" newAmount="70" />
-                    <OwnerCard ownerId="B2" actualAmount="40" newAmount="70" />
-                    <OwnerCard ownerId="A4" actualAmount="40" newAmount="70" />
-                    <OwnerCard ownerId="A5" actualAmount="40" newAmount="70" />
-                </div>
+                    <div className="margin-top-28">
+                        <SearchBar />
+                    </div>
+                    <div className="margin-top-28">
+                        <OwnerCard ownerId="A1" actualAmount="40" newAmount="70" />
+                        <OwnerCard ownerId="A2" actualAmount="40" newAmount="70" />
+                        <OwnerCard ownerId="B1" actualAmount="40" newAmount="70" />
+                        <OwnerCard ownerId="B2" actualAmount="40" newAmount="70" />
+                        <OwnerCard ownerId="A4" actualAmount="40" newAmount="70" />
+                        <OwnerCard ownerId="A5" actualAmount="40" newAmount="70" />
+                    </div>
                 </Flex>
             </main>
+            <Footer />
         </>
     )
 };
