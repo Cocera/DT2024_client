3b5131123d1a9ba140b151bef18e1ee9841e3d01
Cocera <alcocera@gmail.com>
2024-01-17 01:28:18 +0100

    img fixed profileImage

--- src/components/ProfileImage/ProfileImage.jsx
+++ src/components/ProfileImage/ProfileImage.jsx
@@ -5,13 +5,16 @@ import { Image } from 'antd';
 const ProfileImage = (params) => {
     if (!params.src) {
         return (
-            <Image src='https://estudiodaes.es/wp-content/uploads/2020/07/Espai-Verd-1.jpg' width={params.width} height={params.height} preview={false} />
+            <div className="profile-image-rounded">
+                <Image src='https://estudiodaes.es/wp-content/uploads/2020/07/Espai-Verd-1.jpg' width={params.width} height={params.height} preview={false} />
+            </div>
+
         )
     }
     return (
-        <>
+        <div className="profile-image-rounded">
             <Image src={params.src} width={params.width} height={params.height} preview={false} />
-        </>
+        </div>
     )
 };
 
